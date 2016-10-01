4bcf597b2581ae24c577b85859a549e3b0a0bf18
Sophia <swoozie14.com>
2016-10-01 14:44:56 -0700

    Pass all tests

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,36 @@
 #write your code here
+def echo(input)
+    input
+end
+
+def shout(input)
+    input.upcase
+end
+
+def repeat(input, num=2)
+    result = input
+    (num - 1).times do
+        result += " " + input
+    end
+    result
+end
+
+def start_of_word(word, num)
+    word[0,num]
+end
+
+def first_word(words)
+    arr = words.split
+    arr[0]
+end
+
+def titleize(words)
+    arr = words.split
+    arr.each do |word|
+        if (word != "and" && word != "the" && word != "over")
+            word.capitalize!
+        end
+    end
+    arr[0].capitalize!
+    arr.join(" ")
+end
