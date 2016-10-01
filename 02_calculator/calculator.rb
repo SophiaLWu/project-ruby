5d0edc47b214bedcf07c8ef2cf22c2ead31e5387
Sophia <swoozie14.com>
2016-10-01 14:13:27 -0700

    Pass all tests

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
 #write your code here
+def add(x, y)
+    x + y
+end
+
+def subtract(x, y)
+    x - y
+end
+
+def sum(arr)
+    sum = 0
+    arr.each do |x|
+        sum += x
+    end
+    sum
+end
