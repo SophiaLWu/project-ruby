635c67e6287e26e721aeca8421091a4ffd4c21a3
Sophia <swoozie14.com>
2016-10-02 11:09:27 -0700

    Pass all tests

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,23 @@
 #write your code here
+def is_vowel(letter)
+    letter == "a" || letter == "e" || letter == "i" ||
+    letter == "o" || letter == "u"
+end
+
+def translate(input)
+    words = input.split
+    words.map! do |word|
+        word = word.split("")
+        until is_vowel(word[0])
+            first_shift = word.shift
+            word << first_shift
+            if (first_shift == "q" && word[0] == "u") # handles "qu" case
+                second_shift = word.shift
+                word << second_shift
+            end
+        end
+        word << "ay"
+        word.join
+    end
+    words.join(" ")
+end
